78fe3d9d92feda0f2df331d2424e605456c916af
Julio Piza <jpiza@wi-mobile.com>
2019-04-09 15:02:02 -0500

    Cambios

---
+++ app/src/main/java/com/android/compraencasa/CrearUsuarioActivity.java
@@ -0,0 +1,13 @@
+package com.android.compraencasa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrearUsuarioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_usuario);
+    }
+}
